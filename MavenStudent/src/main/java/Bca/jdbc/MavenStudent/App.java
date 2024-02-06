da515370b4b43b9b667ae0d8993eea6559473472
PriyaShandilya <priyashandilya682@gmail.com>
2024-02-06 12:54:20 +0530

    maven

---
+++ MavenStudent/src/main/java/Bca/jdbc/MavenStudent/App.java
@@ -0,0 +1,13 @@
+package Bca.jdbc.MavenStudent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
